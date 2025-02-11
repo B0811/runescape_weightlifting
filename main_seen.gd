95fe3bebaeb55030be350dcff4a190baa5064c8a
B0811 <typhloziken@gmail.com>
2025-02-11 00:49:48 -0500

    redo

--- main_seen.gd
+++ main_seen.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
 
+@onready var overhead = $overhead
+@onready var squat = $squat
+@onready var bench = $bench
+
+@onready var squatbar = $squatbar
+@onready var benchbar = $benchbar
+@onready var overheadbar = $overheadbar
+
+
 
 
 
